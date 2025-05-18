bbad40ca4961bcc627409a592738eb4465d92fc5
Blitzpl <72107580+Blitzpl@users.noreply.github.com>
2025-05-18 18:34:53 +0200

    Add files via upload

---
+++ dao/AbstractDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import error.handlers.ErrorHandler;
+import utils.Message;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+
+public abstract class AbstractDAO {
+    protected final Connection conn;
+    protected final Message message;
+    protected final ErrorHandler errorHandler;
+
+    public AbstractDAO(Connection conn) {
+        this.conn = conn;
+        this.message = new Message();
+        this.errorHandler = new ErrorHandler();
+    }
+
+}
